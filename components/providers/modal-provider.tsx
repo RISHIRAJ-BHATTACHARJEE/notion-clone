79546e92d449304cb3d20f04d875e9dd69385d66
Rishiraj Bhattacharjee <rishirajbhattacharjee974@gmail.com>
2024-08-07 00:10:42 +0530

    initial commit

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,25 @@
+'use client'	
+
+import { useEffect, useState } from "react"
+import { SettingsModal } from "../modals/SettingsModal"
+import { CoverImageModal } from "../modals/CoverImageModal"
+
+export function ModalProvider () {
+
+  const [isMounted,setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  },[])
+
+  if (!isMounted) {
+    return null
+  }
+
+  return (
+    <>
+      <SettingsModal/>
+      <CoverImageModal/>
+    </>
+)
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://civil-trout-86.clerk.accounts.dev/",
+            applicationID: "convex"
+        }
+    ]
+}
